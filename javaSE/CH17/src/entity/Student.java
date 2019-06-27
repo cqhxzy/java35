e1c7b176c80942515e03d4e853ce37dcef434b8d
nick <nickellp@yeah.net>
2019-06-27 09:56:05 +0800

    新增实体类Student

---
+++ javaSE/CH17/src/entity/Student.java
@@ -0,0 +1,60 @@
+package entity;
+
+import java.util.Objects;
+
+/**
+ * 一个学生的实体类
+ */
+public class Student {
+    //shift + enter  使光标移动到下一行
+    private String name;
+    private Integer age;
+
+    //alt + insert
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) &&
+                Objects.equals(age, student.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
