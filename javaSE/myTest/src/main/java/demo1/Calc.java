695d5899a7a5888e4f1b5a9766d369558deff2e6
nick <nickellp@yeah.net>
2019-07-25 11:55:21 +0800

    单元测试

---
+++ javaSE/myTest/src/main/java/demo1/Calc.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Calc {
+    public int add(int num1,int num2){
+        return num1 + num2;
+    }
+
+    public int mul(int num1,int num2){
+        return num1 * num2;
+    }
+}
---
+++ javaSE/myTest/src/main/java/demo1/UserDao.java
@@ -0,0 +1,8 @@
+package demo1;
+
+public interface UserDao {
+    /**
+     * 保存的方法
+     */
+    void save();
+}
